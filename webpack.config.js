2b9602bccf979ca6b5af09f060d02ebe33716719
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-03-24 10:12:36 -0500

    Add base files

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devServer: {
+    static: "./dist",
+    watchFiles: ["src/*.html"],
+    hot: true,
+  },
+  watch: true,
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Todo List",
+      template: "./src/template.html",
+      filename: "./index.html",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "/",
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
