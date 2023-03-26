a2b29073af3af1ba0445eccdc81d50c113cb411d
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-03-25 22:32:01 -0500

    Create background function to change img

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./style.css";
 
 const zipBtn = document.querySelector("#zipBtn");
+const DEFAULT_ZIPCODE = "35244";
 
 async function myFetcher(zipcode) {
   const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=66cb727fba2c444da1b31139232203&q=${zipcode}&aqi=no`, { mode: "cors" });
@@ -20,9 +21,23 @@ function updateWeather(weatherData) {
   humidity.innerHTML = weatherData.current.humidity;
 }
 
+function background(weatherData) {
+  const isDay = weatherData.current.is_day;
+  if (isDay == 0) {
+    document.body.classList.add("night");
+  }
+}
+
+window.addEventListener("load", async () => {
+  const weatherData = await myFetcher(DEFAULT_ZIPCODE);
+  updateWeather(weatherData);
+  console.log(weatherData);
+});
+
 zipBtn.addEventListener("click", async () => {
   const zip = document.getElementById("zip");
   const zipcode = zip.value;
   const weatherData = await myFetcher(zipcode);
   updateWeather(weatherData);
+  background(weatherData);
 });
