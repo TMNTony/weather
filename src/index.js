fce7277bba7b39385a03ba8f90f39f3ee1d4002a
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-03-25 22:06:16 -0500

    Rewrite and separate fetcher

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 import "./style.css";
 
-const zip = document.getElementById("zip");
-const result = document.querySelector(".result");
 const zipBtn = document.querySelector("#zipBtn");
-const temp = document.querySelector(".temp");
-const city = document.querySelector(".city");
 
 async function myFetcher(zipcode) {
   const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=66cb727fba2c444da1b31139232203&q=${zipcode}&aqi=no`, { mode: "cors" });
   const weatherData = await response.json();
-  temp.innerHTML = weatherData.current.temp_f;
-  city.innerHTML = weatherData.location.name;
-
-  console.log(weatherData);
-  console.log(weatherData.current.temp_f);
+  return weatherData;
 }
 
-zipBtn.addEventListener("click", () => {
-  const zipcode = zip.value;
-  myFetcher(zipcode);
-});
+function updateWeather(weatherData) {
+  const temp = document.querySelector(".temp");
+  const city = document.querySelector(".city");
+  const wind = document.querySelector(".wind");
+  const humidity = document.querySelector(".humidity");
 
-myFetcher(35244);
+  temp.innerHTML = weatherData.current.temp_f;
+  city.innerHTML = weatherData.location.name;
+  wind.innerHTML = weatherData.current.wind_mph;
+  humidity.innerHTML = weatherData.current.humidity;
+}
+
+zipBtn.addEventListener("click", async () => {
+  const zip = document.getElementById("zip");
+  const zipcode = zip.value;
+  const weatherData = await myFetcher(zipcode);
+  updateWeather(weatherData);
+});
